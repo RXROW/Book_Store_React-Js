d6dbac455e6543f3f37d2f61c92a64a05d707f6a
“Eslam“ <aslamalmswdya@gmail.com>
2024-04-03 00:45:28 +0200

    Books Slider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
  
 import './App.css';
+import BookSlider from './components/book-slider/BookSlider';
 import Header from './components/header/Header';
 import Services from './components/services/Services';
 import Slider from './components/slider/Slider';
-
+import {books} from '../src/data/books'
 function App() {
   return (
     <div className="App">
       <Header/>
       <Slider/>
       <Services/>
+      <BookSlider data={books}/>
  
 
      
---
+++ src/components/book-slider/BookSlider.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './book-slider.css';
+
+const BookSlider = ({ data }) => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const lengthOfData = data.length;
+
+  const handleClick = (dir) => {
+    if (dir === "left") {
+      setSlideIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : lengthOfData - 1));
+    } else {
+      setSlideIndex((prevIndex) => (prevIndex < lengthOfData - 1 ? prevIndex + 1 : 0));
+    }
+  };
+
+  return (
+    <div className='book-slide-container'>
+      {slideIndex > 0 && (
+        <i onClick={() => handleClick("left")} className="bi bi-chevron-left book-slide-left-arrow"></i>
+      )}
+      <div style={{ transform: `translateX(${slideIndex * -220}px)` }} className="book-slider-wrapper">
+        {data.map((item) => (
+          <div key={item.id} className='book-slide-item'>
+            <img src={`/books/${item.image}`} alt={item.title} className='book-slide-item-img' />
+            <h1 className='book-slide-item-title'>{item.title}</h1>
+            <p className="rating">Rating</p>
+            <p className="book-slide-item-price">${item.price}</p>
+            <div className="book-slide-icon-wrapper">
+              <i className="bi bi-eye-fill"></i>
+              <i className="bi bi-cart-plus"></i>
+            </div>
+          </div>
+        ))}
+      </div>
+      {slideIndex < lengthOfData - 1 && (
+        <i onClick={() => handleClick("right")} className="bi bi-chevron-right book-slide-right-arrow"></i>
+      )}
+    </div>
+  );
+};
+
+export default BookSlider;
